2c294a61b0fe745d9e11640ffe0ea08546caca85
Nyesh Dotson <dotsonnyesh@gmail.com>
2023-03-27 14:26:35 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Nyesh Dotson'
+const welcomeMessage = 'Welcome to Flatbook, ' + 'Nyesh Dotson' + '!'
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!'
+const shortGreeting = 'Welcome, ' + 'N' + '!'
